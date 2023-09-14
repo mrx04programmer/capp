58e1fe4679e7e3d24e84726a8c2952d98f26f063
mrx04 <mjuandavid901@gmail.com>
2023-09-14 14:23:22 -0500

    setup file
    setup 1.0.0 - stable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zapp',
+    version='1.0.0',
+    description='Cyber Apps Suite',
+    author='mrx04programmer',
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies
+        
+    ],
+)
