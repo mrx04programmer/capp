fb762820446290ef3a2ec9c46651f4d7cc05ccda
mrx04 <mjuandavid901@gmail.com>
2023-09-14 15:22:07 -0500

    setup file
    version 1.0.1 & scapy added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zapp',
-    version='1.0.0',
+    version='1.0.1',
     description='Cyber Apps Suite',
     author='mrx04programmer',
     packages=find_packages(),
